83d584772b4c29ea183ab0d12e3f97069d7dee17
jonathan <cliffjonathan66@gmail.com>
2025-03-20 14:38:57 +0300

    work

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+// Function to create multiple employee records from an array of arrays
+function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+}
+
+// Function to add a timeIn event to an employee's record
+function createTimeInEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        date: date,
+        hour: parseInt(hour, 10)
+    });
+    return employee;
+}
+
+// Function to add a timeOut event to an employee's record
+function createTimeOutEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        date: date,
+        hour: parseInt(hour, 10)
+    });
+    return employee;
+}
+
+// Function to calculate hours worked on a specific date
+function hoursWorkedOnDate(employee, date) {
+    let timeIn = employee.timeInEvents.find(event => event.date === date);
+    let timeOut = employee.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100; // Converts to hours
+}
+
+// Function to calculate wages earned on a specific date
+function wagesEarnedOnDate(employee, date) {
+    return hoursWorkedOnDate(employee, date) * employee.payPerHour;
+}
+
+// Function to calculate total wages for an employee
+function allWagesFor(employee) {
+    return employee.timeInEvents.reduce((total, event) => {
+        return total + wagesEarnedOnDate(employee, event.date);
+    }, 0);
+}
+
+// Function to calculate total payroll for all employees
+function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => total + allWagesFor(employee), 0);
+}
+
+// Exporting the functions for testing
+module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    calculatePayroll
+};
+
